5288c2665090d00233909f77bbcaf9255701db88
Eduardo <yuunoev@gmail.com>
2025-10-18 21:57:55 -0300

    m

--- termo1/index.js
+++ termo1/index.js
@@ -1,9 +1,38 @@
 let body = document.querySelector('body')
 let gameState = 'Ingame'
-let word = 'paraua'.toUpperCase()
-word = word.split('')
-let wordMap = new Map()
+let word = []
 
+let isWordLoaded = false
+let wordPromise = loadWord().then((palavraFinal) => {
+    word = palavraFinal.toUpperCase().split('')
+    isWordLoaded = true
+})
+
+let checkPromise = setInterval(() => {
+    if(!isWordLoaded){
+        console.log('carregando')
+    }else {
+        console.log('carregou')
+        clearInterval(checkPromise)
+    }
+}, 500)
+async function loadWord (){
+    let palavra = "";
+    try{
+        palavra = await fetch('https://api.dicionario-aberto.net/random')
+        let palavra1 = await palavra.json()
+        let palavra2 = palavra1.word.toUpperCase()
+        palavra2 = palavra2.split('')
+        if(palavra2.length !== 6){
+            return loadWord()
+        }
+        return await palavra1.word
+    }catch(error){
+        console.log(error)
+    }
+}
+
+let wordMap = new Map()
 function verifyGreen(atualBox, index){
     if(atualBox.innerHTML === word[index] && wordMap.has(atualBox.innerHTML)){
         atualBox.classList.add('correct')
@@ -93,9 +122,11 @@ function changeRow(){
 function loadRow(){
     return document.querySelector('.editing')
 }
+
 body.addEventListener('keyup',(tecla)=>{
     if(gameState !== 'Ingame') return
     let teclaP = tecla.key
+    console.log(word)
     let currentBox = loadRow()
     if (!currentBox) return;
     let nextBox = currentBox.nextElementSibling
@@ -110,7 +141,10 @@ body.addEventListener('keyup',(tecla)=>{
         if(currentBox.innerHTML !== "" && previousBox !== null){
             currentBox.classList.remove('editing')
             previousBox.classList.add('editing')
-            previousBox.innerHTML = ""
+            currentBox.innerHTML = ""
+        }else if (currentBox.innerHTML === "" && previousBox !== null){
+            currentBox.classList.remove('editing')
+            previousBox.classList.add('editing')
         }else{
             currentBox.innerHTML = ""
         }
